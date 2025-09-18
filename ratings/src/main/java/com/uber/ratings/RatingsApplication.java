7b6ff81894cc1cab5bf1a109e18fbcde3c2eb1ab
Kiran Kumar <180952443+bkk07@users.noreply.github.com>
2025-09-18 09:21:38 +0000

    Completed Upto rating service

---
+++ ratings/src/main/java/com/uber/ratings/RatingsApplication.java
@@ -0,0 +1,13 @@
+package com.uber.ratings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingsApplication.class, args);
+    }
+
+}
