d7edec759f1b91a345c15803b3afaec5312a55d0
Kiran Kumar <180952443+bkk07@users.noreply.github.com>
2025-09-19 09:43:50 +0000

    Completed upto the Payment Service

---
+++ paymentservice/src/test/java/com/uber/paymentservice/PaymentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uber.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
